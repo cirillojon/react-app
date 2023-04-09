9014b3d5745bf8b8d00d93054bdf158ca524c72c
Jon <104590083+cirillojon@users.noreply.github.com>
2023-04-09 11:03:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
-import React, { Component } from "react"
-//import logo from "./logo.svg"
-import "./App.css"
-import Routes from "./routes"
-import Navbar from "./Navbar"
-
-// class LambdaDemo extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = { loading: false, msg: null }
-//   }
-
-//   handleClick = api => e => {
-//     e.preventDefault()
-
-//     this.setState({ loading: true })
-//     fetch("/.netlify/functions/" + api)
-//       .then(response => response.json())
-//       .then(json => this.setState({ loading: false, msg: json.msg }))
-//   }
-
-//   render() {
-//     const { loading, msg } = this.state
-
-//     return (
-//       <p>
-//         <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-//         <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-//         <br />
-//         <span>{msg}</span>
-//       </p>
-//     )
-//   }
-// }
+import React, { Component } from "react";
+import "./App.css";
+import Routes from "./routes";
+import Navbar from "./Navbar";
+import LambdaDemo from "./LambdaDemo"; // Make sure the path is correct
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Routes />
-      <Navbar />
-    </div>
-    )
+        <Navbar />
+        <Routes />
+        <LambdaDemo />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
