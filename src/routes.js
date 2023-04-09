3d3428cfae17698030e4a49c64ff1ae4f643e6e7
Jon <104590083+cirillojon@users.noreply.github.com>
2023-04-09 15:14:03 -0400

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './appPages/HomePage';
-import AboutPage from './appPages/AboutPage';
+import Minigames from './appPages/Minigames';
 import ContactPage from './appPages/ContactPage';
 import Game1 from './appPages/Game1';
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
-    <Route path="/about" element={<AboutPage />} />
+    <Route path="/minigames" element={<Minigames />} />
     <Route path="/contact" element={<ContactPage />} />
     <Route path="/Game1" element={<Game1 />} />
   </Routes>
