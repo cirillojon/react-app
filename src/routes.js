f1542369ae7998d49b6625b58db5d34d907ffe36
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-08 20:05:45 -0400

    changed pages to appPages

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
+import HomePage from './appPages/HomePage';
+import AboutPage from './appPages/AboutPage';
+import ContactPage from './appPages/ContactPage';
 
 const Routes = () => (
   <Router>
