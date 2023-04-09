6c4375d98ad0449e8a40ec1b42867143089b0671
Jon <104590083+cirillojon@users.noreply.github.com>
2023-04-09 14:07:53 -0400

    Update AboutPage.js

--- src/appPages/AboutPage.js
+++ src/appPages/AboutPage.js
@@ -1,5 +1,58 @@
 import React, { useState } from "react";
 
+const choices = ["rock", "paper", "scissors"];
+
+const getRandomChoice = () => {
+  return choices[Math.floor(Math.random() * choices.length)];
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return "It's a tie!";
+  }
+
+  if (
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "You win!";
+  }
+
+  return "You lose!";
+};
+
+const RockPaperScissors = () => {
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState("");
+
+  const handleUserChoice = (choice) => {
+    setUserChoice(choice);
+    const computerSelectedChoice = getRandomChoice();
+    setComputerChoice(computerSelectedChoice);
+    setResult(determineWinner(choice, computerSelectedChoice));
+  };
+
+  return (
+    <div>
+      <h2>Rock-Paper-Scissors</h2>
+      <div>
+        {choices.map((choice) => (
+          <button key={choice} onClick={() => handleUserChoice(choice)}>
+            {choice}
+          </button>
+        ))}
+      </div>
+      {userChoice && computerChoice && (
+        <p>
+          You chose {userChoice}, computer chose {computerChoice}. {result}
+        </p>
+      )}
+    </div>
+  );
+};
+
 const AboutPage = () => {
   const [numberToGuess] = useState(Math.floor(Math.random() * 10) + 1);
   const [userGuess, setUserGuess] = useState("");
@@ -26,6 +79,7 @@ const AboutPage = () => {
     <div>
       <h1>About Us</h1>
       <p>We are a company dedicated to providing the best services to our clients.</p>
+
       <h2>Number Guessing Game</h2>
       <p>Guess a number between 1 and 10:</p>
       <form onSubmit={handleSubmit}>
@@ -33,6 +87,8 @@ const AboutPage = () => {
         <button type="submit">Submit</button>
       </form>
       {feedback && <p>{feedback}</p>}
+
+      <RockPaperScissors />
     </div>
   );
 };
