2e231b5d7b835802f0236431bab3d557cc416abb
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 16:04:56 -0400

    Update ThreeJsHomePage.js

--- src/appPages/ThreeJsHomePage.js
+++ src/appPages/ThreeJsHomePage.js
@@ -1,9 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Cylinder, Cone } from '@react-three/drei';
-import * as THREE from 'three'; 
-import { PointLight } from '@react-three/drei';
-
+import * as THREE from 'three'; // import THREE
 
 const ThreeJsHomePage = () => {
   const spaceshipRef = useRef();
@@ -35,52 +33,15 @@ const ThreeJsHomePage = () => {
     };
   }, []);
 
-  useFrame(({ clock, viewport }) => {
+  useFrame(({ clock }) => {
     if (spaceshipRef.current) {
       const speed = 0.1;
       const { x, y } = rotate;
       spaceshipRef.current.rotation.set(x, y + clock.elapsedTime * 0.5, 0);
       spaceshipRef.current.translateZ(-speed);
-  
-      // Get the bounding box of the spaceship
-      const boundingBox = new THREE.Box3().setFromObject(spaceshipRef.current);
-  
-      // Check if the spaceship is going off-screen on the left or right
-      if (boundingBox.min.x < viewport.width / -2 || boundingBox.max.x > viewport.width / 2) {
-        // If so, reverse the x-velocity
-        speed *= -1;
-        spaceshipRef.current.translateZ(speed * 2);
-  
-        // Randomize the y-velocity
-        spaceshipRef.current.translateY((Math.random() - 0.5) * 2);
-  
-        // Rotate the spaceship
-        spaceshipRef.current.rotation.set(
-          spaceshipRef.current.rotation.x,
-          spaceshipRef.current.rotation.y + Math.PI,
-          spaceshipRef.current.rotation.z
-        );
-      }
-  
-      // Check if the spaceship is going off-screen on the top or bottom
-      if (boundingBox.min.y < viewport.height / -2 || boundingBox.max.y > viewport.height / 2) {
-        // If so, reverse the y-velocity
-        speed *= -1;
-        spaceshipRef.current.translateZ(speed * 2);
-  
-        // Randomize the x-velocity
-        spaceshipRef.current.translateX((Math.random() - 0.5) * 2);
-  
-        // Rotate the spaceship
-        spaceshipRef.current.rotation.set(
-          spaceshipRef.current.rotation.x,
-          spaceshipRef.current.rotation.y + Math.PI,
-          spaceshipRef.current.rotation.z
-        );
-      }
     }
   });
-  
+
   return (
     <group ref={spaceshipRef}>
       <Cylinder args={[0.5, 0.5, 2, 32]} castShadow>
@@ -89,10 +50,11 @@ const ThreeJsHomePage = () => {
       <Cone args={[0.5, 1, 32]} position={[0, 1, 0]} castShadow>
         <meshStandardMaterial attach="material" map={reflectiveTexture} />
       </Cone>
-      <PointLight color="white" position={[0, 1, 0]} intensity={1} />
+      <directionalLight color="white" intensity={1} position={[0, 1, 0]} />
     </group>
   );
 };
+
 const metalTexture = new THREE.TextureLoader().load('/metal.jpg');
 const reflectiveTexture = new THREE.TextureLoader().load('/reflective.jpg');
 
