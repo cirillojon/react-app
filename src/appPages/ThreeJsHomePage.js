2112f1542f8a985773fd95214f3ddcdab1df6ed2
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 15:45:06 -0400

    attempted move fix

--- src/appPages/ThreeJsHomePage.js
+++ src/appPages/ThreeJsHomePage.js
@@ -4,29 +4,10 @@ import { Cylinder, Cone } from '@react-three/drei';
 
 const ThreeJsHomePage = () => {
   const spaceshipRef = useRef();
-  // const [move, setMove] = useState({ forward: false, backward: false, left: false, right: false, up: false, down: false });
   const [rotate, setRotate] = useState({ x: 0, y: 0 });
   const mouseRef = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === 'w') setMove((m) => ({ ...m, forward: true }));
-      if (event.key === 's') setMove((m) => ({ ...m, backward: true }));
-      if (event.key === 'a') setMove((m) => ({ ...m, left: true }));
-      if (event.key === 'd') setMove((m) => ({ ...m, right: true }));
-      if (event.key === ' ') setMove((m) => ({ ...m, up: true }));
-      if (event.key === 'Shift') setMove((m) => ({ ...m, down: true }));
-    };
-
-    const handleKeyUp = (event) => {
-      if (event.key === 'w') setMove((m) => ({ ...m, forward: false }));
-      if (event.key === 's') setMove((m) => ({ ...m, backward: false }));
-      if (event.key === 'a') setMove((m) => ({ ...m, left: false }));
-      if (event.key === 'd') setMove((m) => ({ ...m, right: false }));
-      if (event.key === ' ') setMove((m) => ({ ...m, up: false }));
-      if (event.key === 'Shift') setMove((m) => ({ ...m, down: false }));
-    };
-
     const handleMouseMove = (event) => {
       const { x, y } = mouseRef.current;
       const dx = event.clientX - x;
@@ -43,18 +24,13 @@ const ThreeJsHomePage = () => {
       mouseRef.current = { x: event.touches[0].clientX, y: event.touches[0].clientY };
     };
 
-    window.addEventListener('keydown', handleKeyDown);
-    window.addEventListener('keyup', handleKeyUp);
     window.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('touchmove', handleTouchMove);
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-      window.removeEventListener('keyup', handleKeyUp);
       window.removeEventListener('mousemove', handleMouseMove);
       window.removeEventListener('touchmove', handleTouchMove);
     };
   }, []);
-
 
   useFrame(({ clock }) => {
     if (spaceshipRef.current) {
@@ -65,18 +41,16 @@ const ThreeJsHomePage = () => {
     }
   });
   
-  
   return (
-  <group ref={spaceshipRef}>
-  <Cylinder args={[0.5, 0.5, 2, 32]} castShadow>
-  <meshBasicMaterial attach="material" color="yellow" />
-  </Cylinder>
-  <Cone args={[0.5, 1, 32]} position={[0, 1, 0]} castShadow>
-  <meshBasicMaterial attach="material" color="red" />
-  </Cone>
-  </group>
+    <group ref={spaceshipRef}>
+      <Cylinder args={[0.5, 0.5, 2, 32]} castShadow>
+        <meshBasicMaterial attach="material" color="yellow" />
+      </Cylinder>
+      <Cone args={[0.5, 1, 32]} position={[0, 1, 0]} castShadow>
+        <meshBasicMaterial attach="material" color="red" />
+      </Cone>
+    </group>
   );
-  };
-  
-  export default ThreeJsHomePage;
-    
+};
+
+export default ThreeJsHomePage;
