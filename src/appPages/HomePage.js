d3367656572c124af02674f9c3fc566386c71ed4
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 15:08:42 -0400

    Update HomePage.js

--- src/appPages/HomePage.js
+++ src/appPages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Stars, AmbientLight, PointLight } from '@react-three/drei';
+import { Canvas, ambientLight } from '@react-three/fiber';
+import { Stars, PointLight } from '@react-three/drei';
 import ThreeJsHomePage from './ThreeJsHomePage';
 
 const HomePage = () => {
@@ -10,8 +10,8 @@ const HomePage = () => {
       <p>One day this will be something great.</p>
       <Canvas style={{ background: '#000033' }} camera={{ position: [0, 0, 15], fov: 75 }}>
       <Stars />
-      <AmbientLight intensity={0.5} />
-      <PointLight position={[10, 10, 10]} intensity={1} /> {/* Add PointLight */}
+      <ambientLight intensity={0.5} /> {/* Update the component usage */}
+      <PointLight position={[10, 10, 10]} intensity={1} />
       <ThreeJsHomePage />
     </Canvas>
     </div>
