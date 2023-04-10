695f50c7e19ac5131b3643f02ec3de7c27cf6398
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 14:18:00 -0400

    Update ThreeJsHomePage.js

--- src/appPages/ThreeJsHomePage.js
+++ src/appPages/ThreeJsHomePage.js
@@ -1,22 +1,6 @@
-// ThreeJsHomePage.js
 import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { CylinderBufferGeometry, ConeBufferGeometry } from 'three';
-
-const Spaceship = () => {
-  return (
-    <>
-      <mesh>
-        <CylinderBufferGeometry attach="geometry" args={[0.5, 0.5, 2, 32]} />
-        <meshBasicMaterial attach="material" color="yellow" />
-      </mesh>
-      <mesh position={[0, 0, 1]}>
-        <ConeBufferGeometry attach="geometry" args={[0.5, 1, 32]} />
-        <meshBasicMaterial attach="material" color="yellow" />
-      </mesh>
-    </>
-  );
-};
+import { Cylinder, Cone } from '@react-three/drei';
 
 const ThreeJsHomePage = () => {
   const spaceshipRef = useRef();
@@ -24,7 +8,7 @@ const ThreeJsHomePage = () => {
   useFrame((state) => {
     if (spaceshipRef.current) {
       const time = state.clock.getElapsedTime();
-      spaceshipRef.current.position.x = Math.sin(time) * 5;
+      spaceshipRef.current.position.x = (Math.sin(time) * 10) - 5;
       spaceshipRef.current.position.y = Math.cos(time) * 2;
       spaceshipRef.current.rotation.z = Math.PI / 2 + Math.sin(time) * 0.1;
     }
@@ -32,7 +16,12 @@ const ThreeJsHomePage = () => {
 
   return (
     <group ref={spaceshipRef}>
-      <Spaceship />
+      <Cylinder args={[0.5, 0.5, 2, 32]} castShadow>
+        <meshBasicMaterial attach="material" color="yellow" />
+      </Cylinder>
+      <Cone args={[0.5, 1, 32]} position={[0, 1, 0]} castShadow>
+        <meshBasicMaterial attach="material" color="red" />
+      </Cone>
     </group>
   );
 };
