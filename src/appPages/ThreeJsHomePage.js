21a17af8cf7f45e409775f1ff64a86d0eac5ce59
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 15:28:47 -0400

    Update ThreeJsHomePage.js

--- src/appPages/ThreeJsHomePage.js
+++ src/appPages/ThreeJsHomePage.js
@@ -1,11 +1,12 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Cylinder, Cone } from '@react-three/drei';
-// import SmokeEffect from './SmokeEffect';
 
 const ThreeJsHomePage = () => {
   const spaceshipRef = useRef();
   const [move, setMove] = useState({ forward: false, backward: false, left: false, right: false, up: false, down: false });
+  const [rotate, setRotate] = useState({ x: 0, y: 0 });
+  const mouseRef = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
     const handleKeyDown = (event) => {
@@ -26,38 +27,54 @@ const ThreeJsHomePage = () => {
       if (event.key === 'Shift') setMove((m) => ({ ...m, down: false }));
     };
 
+    const handleMouseMove = (event) => {
+      const { x, y } = mouseRef.current;
+      const dx = event.clientX - x;
+      const dy = event.clientY - y;
+      setRotate((r) => ({ x: r.x - dy * 0.01, y: r.y - dx * 0.01 }));
+      mouseRef.current = { x: event.clientX, y: event.clientY };
+    };
+
+    const handleTouchMove = (event) => {
+      const { x, y } = mouseRef.current;
+      const dx = event.touches[0].clientX - x;
+      const dy = event.touches[0].clientY - y;
+      setRotate((r) => ({ x: r.x - dy * 0.01, y: r.y - dx * 0.01 }));
+      mouseRef.current = { x: event.touches[0].clientX, y: event.touches[0].clientY };
+    };
+
     window.addEventListener('keydown', handleKeyDown);
     window.addEventListener('keyup', handleKeyUp);
-
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('touchmove', handleTouchMove);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
       window.removeEventListener('keyup', handleKeyUp);
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('touchmove', handleTouchMove);
     };
   }, []);
 
-  useFrame(() => {
-    if (spaceshipRef.current) {
-      const speed = 0.1;
-      if (move.forward) spaceshipRef.current.position.z -= speed;
-      if (move.backward) spaceshipRef.current.position.z += speed;
-      if (move.left) spaceshipRef.current.position.x -= speed;
-      if (move.right) spaceshipRef.current.position.x += speed;
-      if (move.up) spaceshipRef.current.position.y += speed;
-      if (move.down) spaceshipRef.current.position.y -= speed;
-    }
+  useFrame(({ clock }) => {
+  if (spaceshipRef.current) {
+  const speed = 0.1;
+  const { x, y } = rotate;
+  spaceshipRef.current.rotation.set(x, y + clock.elapsedTime * 0.5, 0);
+  spaceshipRef.current.translateZ(-speed);
+  }
   });
-
+  
   return (
-    <group ref={spaceshipRef}>
-      <Cylinder args={[0.5, 0.5, 2, 32]} castShadow>
-        <meshBasicMaterial attach="material" color="yellow" />
-      </Cylinder>
-      <Cone args={[0.5, 1, 32]} position={[0, 1, 0]} castShadow>
-      <meshBasicMaterial attach="material" color="red" />
-      </Cone>
-      {/* <SmokeEffect count={500} color="white" /> */}
-    </group>
+  <group ref={spaceshipRef}>
+  <Cylinder args={[0.5, 0.5, 2, 32]} castShadow>
+  <meshBasicMaterial attach="material" color="yellow" />
+  </Cylinder>
+  <Cone args={[0.5, 1, 32]} position={[0, 1, 0]} castShadow>
+  <meshBasicMaterial attach="material" color="red" />
+  </Cone>
+  </group>
   );
-};
-
-export default ThreeJsHomePage;
+  };
+  
+  export default ThreeJsHomePage;
+    
