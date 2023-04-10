65ceea4ed9db7186688e4b6b91e45bbebfaf3fec
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 15:59:52 -0400

    added pointlight import

--- src/appPages/ThreeJsHomePage.js
+++ src/appPages/ThreeJsHomePage.js
@@ -2,6 +2,7 @@ import React, { useRef, useState, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Cylinder, Cone } from '@react-three/drei';
 import * as THREE from 'three'; 
+import { PointLight } from '@react-three/drei';
 
 
 const ThreeJsHomePage = () => {
