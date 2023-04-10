43629c1eda03fc9165ae21a7bbf5ebac3d0ad43d
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 11:29:38 -0400

    added threejs

--- src/appPages/HomePage.js
+++ src/appPages/HomePage.js
@@ -1,12 +1,20 @@
+// HomePage.js
 import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import SpinningCube from './SpinningCube';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <p>One day this will be something great. </p>
+      <p>One day this will be something great.</p>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <SpinningCube />
+      </Canvas>
     </div>
   );
-}
+};
 
 export default HomePage;
---
+++ src/appPages/SpinningCube.js
@@ -0,0 +1,23 @@
+// SpinningCube.js
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+
+const SpinningCube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+};
+
+export default SpinningCube;
