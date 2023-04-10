f416193f832b1988ba867748a49d02907ab917ac
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 11:47:26 -0400

    Update HomePage.js

--- src/appPages/HomePage.js
+++ src/appPages/HomePage.js
@@ -6,15 +6,15 @@ import ThreeJsHomePage from './ThreeJsHomePage';
 
 const HomePage = () => {
   return (
-    <div>
+    <div style={{ width: '100vw', height: '100vh' }}>
       <h1>Welcome to the Home Page</h1>
       <p>One day this will be something great.</p>
-      <Canvas>
+      <Canvas style={{ background: '#000033' }} camera={{ position: [0, 0, 10], fov: 75 }}>
         <Stars />
         <ThreeJsHomePage />
       </Canvas>
     </div>
   );
-};
+}
 
 export default HomePage;
