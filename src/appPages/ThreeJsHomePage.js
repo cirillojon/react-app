50dbc7f00c56b82ba58aabeb03f2dbfa94aff11c
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 15:53:32 -0400

    ricochet logic

--- src/appPages/ThreeJsHomePage.js
+++ src/appPages/ThreeJsHomePage.js
@@ -32,12 +32,49 @@ const ThreeJsHomePage = () => {
     };
   }, []);
 
-  useFrame(({ clock }) => {
+  useFrame(({ clock, viewport }) => {
     if (spaceshipRef.current) {
       const speed = 0.1;
       const { x, y } = rotate;
       spaceshipRef.current.rotation.set(x, y + clock.elapsedTime * 0.5, 0);
       spaceshipRef.current.translateZ(-speed);
+  
+      // Get the bounding box of the spaceship
+      const boundingBox = new THREE.Box3().setFromObject(spaceshipRef.current);
+  
+      // Check if the spaceship is going off-screen on the left or right
+      if (boundingBox.min.x < viewport.width / -2 || boundingBox.max.x > viewport.width / 2) {
+        // If so, reverse the x-velocity
+        speed *= -1;
+        spaceshipRef.current.translateZ(speed * 2);
+  
+        // Randomize the y-velocity
+        spaceshipRef.current.translateY((Math.random() - 0.5) * 2);
+  
+        // Rotate the spaceship
+        spaceshipRef.current.rotation.set(
+          spaceshipRef.current.rotation.x,
+          spaceshipRef.current.rotation.y + Math.PI,
+          spaceshipRef.current.rotation.z
+        );
+      }
+  
+      // Check if the spaceship is going off-screen on the top or bottom
+      if (boundingBox.min.y < viewport.height / -2 || boundingBox.max.y > viewport.height / 2) {
+        // If so, reverse the y-velocity
+        speed *= -1;
+        spaceshipRef.current.translateZ(speed * 2);
+  
+        // Randomize the x-velocity
+        spaceshipRef.current.translateX((Math.random() - 0.5) * 2);
+  
+        // Rotate the spaceship
+        spaceshipRef.current.rotation.set(
+          spaceshipRef.current.rotation.x,
+          spaceshipRef.current.rotation.y + Math.PI,
+          spaceshipRef.current.rotation.z
+        );
+      }
     }
   });
   
