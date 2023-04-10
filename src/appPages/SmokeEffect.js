8f14b6681a8af3a78896b2ff3d97a5cf90af46f6
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 14:51:12 -0400

    Update SmokeEffect.js

--- src/appPages/SmokeEffect.js
+++ src/appPages/SmokeEffect.js
@@ -1,16 +1,16 @@
-// import * as THREE from 'three';
-import React, { useMemo } from 'react';
+import React, { useMemo, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 
 const SmokeEffect = ({ count, color }) => {
   const particles = useMemo(() => {
     const temp = [];
     for (let i = 0; i < count; i++) {
       const t = Math.random() * 100;
-      const factor = 20;
+      const factor = Math.random() * 20;
       const speed = 0.01;
-      const xFactor = -5 + Math.random() * 10;
-      const yFactor = -5 + Math.random() * 10;
-      const zFactor = -5 + Math.random() * 10;
+      const xFactor = 0;
+      const yFactor = -1;
+      const zFactor = 0;
       temp.push({
         t,
         factor,
@@ -25,9 +25,27 @@ const SmokeEffect = ({ count, color }) => {
     return temp;
   }, [count]);
 
+  const particleGeometryRef = useRef();
+  const particleMaterialRef = useRef();
+
+  useFrame((state) => {
+    if (particleGeometryRef.current) {
+      const time = state.clock.getElapsedTime();
+      for (let i = 0; i < particles.length; i++) {
+        const particle = particles[i];
+        particle.t += particle.speed * (1 + particle.factor);
+        const positions = particleGeometryRef.current.attributes.position.array;
+        positions[i * 3] = particle.xFactor + Math.sin(particle.t) * particle.factor;
+        positions[i * 3 + 1] = particle.yFactor - particle.t * 0.2;
+        positions[i * 3 + 2] = particle.zFactor + Math.cos(particle.t) * particle.factor;
+      }
+      particleGeometryRef.current.attributes.position.needsUpdate = true;
+    }
+  });
+
   return (
     <points>
-      <bufferGeometry attach="geometry">
+      <bufferGeometry ref={particleGeometryRef} attach="geometry">
         <bufferAttribute
           attachObject={['attributes', 'position']}
           array={new Float32Array(particles.map(({ xFactor, yFactor, zFactor }) => [xFactor, yFactor, zFactor]).flat())}
@@ -35,7 +53,7 @@ const SmokeEffect = ({ count, color }) => {
           itemSize={3}
         />
       </bufferGeometry>
-      <pointsMaterial attach="material" size={0.1} sizeAttenuation color={color} transparent opacity={0.2} />
+      <pointsMaterial ref={particleMaterialRef} attach="material" size={0.1} sizeAttenuation color={color} transparent opacity={0.2} />
     </points>
   );
 };
