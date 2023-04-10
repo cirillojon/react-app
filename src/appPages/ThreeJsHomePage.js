d598f0f95223ab5d172a259337b23a4016d4311d
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 14:10:59 -0400

    expanded theme + spaceship design

--- src/appPages/ThreeJsHomePage.js
+++ src/appPages/ThreeJsHomePage.js
@@ -1,6 +1,32 @@
 // ThreeJsHomePage.js
 import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
+import { Group, CylinderBufferGeometry, ConeBufferGeometry, MeshBasicMaterial, Vector3 } from 'three';
+
+const Spaceship = () => {
+  const material = new MeshBasicMaterial({ color: 'yellow' });
+
+  const body = (
+    <mesh>
+      <CylinderBufferGeometry attach="geometry" args={[0.5, 0.5, 2, 32]} />
+      {material}
+    </mesh>
+  );
+
+  const nose = (
+    <mesh position={new Vector3(0, 0, 1)}>
+      <ConeBufferGeometry attach="geometry" args={[0.5, 1, 32]} />
+      {material}
+    </mesh>
+  );
+
+  return (
+    <Group>
+      {body}
+      {nose}
+    </Group>
+  );
+};
 
 const ThreeJsHomePage = () => {
   const spaceshipRef = useRef();
@@ -8,7 +34,7 @@ const ThreeJsHomePage = () => {
   useFrame((state) => {
     if (spaceshipRef.current) {
       const time = state.clock.getElapsedTime();
-      spaceshipRef.current.position.x = (Math.sin(time) * 5) - 5;
+      spaceshipRef.current.position.x = Math.sin(time) * 5;
       spaceshipRef.current.position.y = Math.cos(time) * 2;
       spaceshipRef.current.rotation.z = Math.PI / 2 + Math.sin(time) * 0.1;
     }
@@ -16,8 +42,7 @@ const ThreeJsHomePage = () => {
 
   return (
     <mesh ref={spaceshipRef}>
-      <coneBufferGeometry attach="geometry" args={[0.5, 2, 32]} />
-      <meshBasicMaterial attach="material" color="yellow" />
+      <Spaceship />
     </mesh>
   );
 };
