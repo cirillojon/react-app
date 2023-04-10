ff88871006a6198e10080b2a8c46ba6ffbfc855e
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-10 15:49:56 -0400

    improved movement

--- src/appPages/ThreeJsHomePage.js
+++ src/appPages/ThreeJsHomePage.js
@@ -6,6 +6,7 @@ const ThreeJsHomePage = () => {
   const spaceshipRef = useRef();
   const [rotate, setRotate] = useState({ x: 0, y: 0 });
   const mouseRef = useRef({ x: 0, y: 0 });
+  const [move, setMove] = useState(false);
 
   useEffect(() => {
     const handleMouseMove = (event) => {
@@ -14,6 +15,7 @@ const ThreeJsHomePage = () => {
       const dy = event.clientY - y;
       setRotate((r) => ({ x: r.x - dy * 0.01, y: r.y - dx * 0.01 }));
       mouseRef.current = { x: event.clientX, y: event.clientY };
+      setMove(true);
     };
 
     const handleTouchMove = (event) => {
@@ -22,6 +24,7 @@ const ThreeJsHomePage = () => {
       const dy = event.touches[0].clientY - y;
       setRotate((r) => ({ x: r.x - dy * 0.01, y: r.y - dx * 0.01 }));
       mouseRef.current = { x: event.touches[0].clientX, y: event.touches[0].clientY };
+      setMove(true);
     };
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -34,13 +37,14 @@ const ThreeJsHomePage = () => {
 
   useFrame(({ clock }) => {
     if (spaceshipRef.current) {
-      const speed = 0.1;
+      const speed = move ? 0.2 : 0.1;
       const { x, y } = rotate;
       spaceshipRef.current.rotation.set(x, y + clock.elapsedTime * 0.5, 0);
       spaceshipRef.current.translateZ(-speed);
+      setMove(false);
     }
   });
-  
+
   return (
     <group ref={spaceshipRef}>
       <Cylinder args={[0.5, 0.5, 2, 32]} castShadow>
