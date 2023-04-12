453453ee17c7631b2a96bad217f76f8c0c64d6a6
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-12 11:52:47 -0400

    iFrame dimensions

--- src/appPages/Game6.js
+++ src/appPages/Game6.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 class UnityGame extends React.Component {
+  componentDidMount() {
+    this.getIframeDimensions();
+  }
+
+  getIframeDimensions() {
+    const iframe = document.getElementById('gameIframe');
+    const width = iframe.offsetWidth;
+    const height = iframe.offsetHeight;
+
+    console.log('Iframe width:', width, 'height:', height);
+  }
+
   render() {
     const gamePath = '/Game1-build/game.html';
 
     return (
       <div>
-        <iframe src={gamePath} title="Unity Game" width="600" height="800"></iframe>
+        <iframe id="gameIframe" src={gamePath} title="Unity Game" width="800" height="800" frameborder="0"></iframe>
       </div>
     );
   }
