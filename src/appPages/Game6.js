56c81ba3f916d87e584c7ad8cfcac0e0b061e59e
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-11 21:49:09 -0400

    Update Game6.js

--- src/appPages/Game6.js
+++ src/appPages/Game6.js
@@ -3,9 +3,9 @@ import React from 'react';
 class UnityGame extends React.Component {
   render() {
     //const gamePath = './Game1-build/game.html'
-    const gamePath = '/appPages/Game1-build/game.html';
+    const gamePath = '/src/appPages/Game1-build/game.html';
 
-    
+
     return (
       <div>
         <iframe src={gamePath} title="Unity Game" width="600" height="800"></iframe>
