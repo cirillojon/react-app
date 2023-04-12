fe76615a157353496a50b090f460a9520612fbac
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-11 21:45:21 -0400

    absolute path

--- src/appPages/Game6.js
+++ src/appPages/Game6.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 class UnityGame extends React.Component {
   render() {
-    const gamePath = './Game1-build/game.html'; 
+    //const gamePath = './Game1-build/game.html'
+    const gamePath = '/appPages/Game1-build/game.html';
 
+    
     return (
       <div>
         <iframe src={gamePath} title="Unity Game" width="600" height="800"></iframe>
