9b201d61973aeb5260394196e11ba21028ca6f65
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-11 20:55:42 -0400

    moving button updates

--- src/appPages/Game5.js
+++ src/appPages/Game5.js
@@ -11,11 +11,16 @@ const MovingButton = () => {
     setPosition({ top: newTop, left: newLeft });
   };
 
+  const handleClick = () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+  };
+
   return (
     <button
       className="moving-button"
       style={{ top: position.top, left: position.left }}
       onMouseOver={handleMouseOver}
+      onClick={handleClick}
     >
       Click me
     </button>
