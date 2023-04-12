c444d048fe56be82747924752e0d5c65a9098aa0
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-04-11 21:52:38 -0400

    moved game.html to public folder

--- src/appPages/Game6.js
+++ src/appPages/Game6.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 class UnityGame extends React.Component {
   render() {
-    //const gamePath = './Game1-build/game.html'
-    const gamePath = '/src/appPages/Game1-build/game.html';
-
+    const gamePath = '/Game1-build/game.html';
 
     return (
       <div>
